1b038682411f3f85a5cb7f9c25430bb8d5fc4eff
Rinaldi <rinaldiamfine@gmail.com>
2021-08-19 19:23:50 +0700

    Notif, compli, resize

--- ambis-reborn/watch Extension/ComplicationView.swift
+++ ambis-reborn/watch Extension/ComplicationView.swift
@@ -8,33 +8,128 @@
 import SwiftUI
 import ClockKit
 
-struct ComplicationViewCircular: View {
-//  @State var appointment: Appointment
-  var body: some View {
-    ZStack {
-        ProgressView(value: (1.0 - 0.9), total: 1.0)
-            .progressViewStyle(CircularProgressViewStyle(tint: .red))
-//        Image("BrandIcon")
-//            .resizable()
-//            .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+func ComplicationCircularCalculate(numerator: Int, denumerator: Int) -> Double {
+    let angle = Double(numerator) / Double(denumerator) * 360
+    return angle - 90
+}
+
+struct TemplateGraphicCircularView: View {
+    
+    @State var nearExpiryItem: Int = 4
+    @State var totalInventory: Int = 16
+    
+    var body: some View {
+        ZStack {
+            if nearExpiryItem == 0 || nearExpiryItem == totalInventory {
+                Circle()
+                    .trim(from: 0.0, to: CGFloat(1 - (Double(nearExpiryItem) / Double(totalInventory))))
+                    .stroke(style: StrokeStyle(lineWidth: 12.0, lineCap: .round, lineJoin: .round))
+                    .opacity(0.3)
+                    .foregroundColor(Color("SuccessColor"))
+                    .rotationEffect(Angle(degrees: ComplicationCircularCalculate(numerator: nearExpiryItem, denumerator: totalInventory)))
+                    .frame(width: 48)
+                Circle()
+                    .trim(from: 0.0, to: CGFloat(Double(nearExpiryItem) / Double(totalInventory)))
+                    .stroke(style: StrokeStyle(lineWidth: 12.0, lineCap: .round, lineJoin: .round))
+                    .foregroundColor(Color("DangerColor"))
+                    .rotationEffect(Angle(degrees: 270))
+                    .frame(width: 48)
+            } else {
+                Circle()
+                    .trim(from: 0.05, to: CGFloat(0.95 - (Double(nearExpiryItem) / Double(totalInventory))))
+                    .stroke(style: StrokeStyle(lineWidth: 10.0, lineCap: .round, lineJoin: .round))
+                    .foregroundColor(Color("SuccessColorTransparent"))
+                    .rotationEffect(Angle(degrees: ComplicationCircularCalculate(numerator: nearExpiryItem, denumerator: totalInventory)))
+                    .frame(width: 48)
+                Circle()
+                    .trim(from: 0.05, to: CGFloat(Double(nearExpiryItem) / Double(totalInventory) - 0.05))
+                    .stroke(style: StrokeStyle(lineWidth: 10.0, lineCap: .round, lineJoin: .round))
+                    .foregroundColor(Color("DangerColor"))
+                    .rotationEffect(Angle(degrees: 270))
+                    .frame(width: 48)
+            }
+            
+            ZStack {
+                Image("BrandIcon")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+            }
+        }
     }
-  }
+}
+
+struct TemplateGraphicExtraLargeCircularView: View {
+    @State var nearExpiryItem: Int = 4
+    @State var totalInventory: Int = 16
+    
+    var body: some View {
+        ZStack {
+            if nearExpiryItem == 0 || nearExpiryItem == totalInventory {
+                Circle()
+                    .trim(from: 0.0, to: CGFloat(1 - (Double(nearExpiryItem) / Double(totalInventory))))
+                    .stroke(style: StrokeStyle(lineWidth: 12.0, lineCap: .round, lineJoin: .round))
+                    .opacity(0.3)
+                    .foregroundColor(Color("SuccessColor"))
+                    .rotationEffect(Angle(degrees: ComplicationCircularCalculate(numerator: nearExpiryItem, denumerator: totalInventory)))
+                    .frame(width: 115, height: 115)
+                Circle()
+                    .trim(from: 0.0, to: CGFloat(Double(nearExpiryItem) / Double(totalInventory)))
+                    .stroke(style: StrokeStyle(lineWidth: 12.0, lineCap: .round, lineJoin: .round))
+                    .foregroundColor(Color("DangerColor"))
+                    .rotationEffect(Angle(degrees: 270))
+                    .frame(width: 115, height: 115)
+            } else {
+                Circle()
+                    .trim(from: 0.025, to: CGFloat(0.975 - (Double(nearExpiryItem) / Double(totalInventory))))
+                    .stroke(style: StrokeStyle(lineWidth: 12.0, lineCap: .round, lineJoin: .round))
+                    .opacity(0.3)
+                    .foregroundColor(Color("SuccessColor"))
+                    .rotationEffect(Angle(degrees: ComplicationCircularCalculate(numerator: nearExpiryItem, denumerator: totalInventory)))
+                    .frame(width: 115, height: 115)
+                Circle()
+                    .trim(from: 0.025, to: CGFloat(Double(nearExpiryItem) / Double(totalInventory) - 0.025))
+                    .stroke(style: StrokeStyle(lineWidth: 12.0, lineCap: .round, lineJoin: .round))
+                    .foregroundColor(Color("DangerColor"))
+                    .rotationEffect(Angle(degrees: 270))
+                    .frame(width: 115, height: 115)
+            }
+            
+            ZStack {
+                Circle()
+                    .fill(Color("GrayColor"))
+                    .frame(width: 80, height: 80)
+                Text(String(nearExpiryItem) + "\nItems")
+                    .multilineTextAlignment(.center)
+                    .font(.system(.headline, design: .rounded))
+            }
+        }
+    }
 }
 
 struct ComplicationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      ZStack {
+          ProgressView(value: (1.0 - 0.9), total: 1.0)
+              .progressViewStyle(CircularProgressViewStyle(tint: .red))
+      }
     }
 }
 
 struct ComplicationView_Previews: PreviewProvider {
     static var previews: some View {
-//        ComplicationView()
-//        ComplicationViewCircular()
         Group {
-          CLKComplicationTemplateGraphicCircularView(
-            ComplicationViewCircular()
-          ).previewContext()
+            CLKComplicationTemplateGraphicExtraLargeCircularView(
+                TemplateGraphicExtraLargeCircularView()
+            )
+            .previewContext()
+            
+//            CLKComplicationTemplateModularLargeTallBody(headerTextProvider: CLKTextProvider(format: "A"), bodyTextProvider: CLKTextProvider(format: "A"))
+//            .previewContext()
+            
+            CLKComplicationTemplateGraphicCircularView(
+                TemplateGraphicCircularView()
+            )
+            .previewContext()
         }
     }
 }
--- ambis-reborn/watch Extension/NotificationView.swift
+++ ambis-reborn/watch Extension/NotificationView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct NotificationView: View {
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader(content: { geometry in
+            VStack(alignment: .center, spacing: 5) {
+                HStack(alignment: .center) {
+                    Image("BrandIcon")
+                        .resizable()
+                        .frame(width: 122, height: 122)
+                }
+                .frame(width: geometry.size.width, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                
+                HStack(alignment: .center) {
+                    Text("Items Expired")
+                        .font(.system(.body, design: .rounded))
+                }
+                
+                HStack(alignment: .center) {
+                    Text("ExpiRemind")
+                        .font(.system(.body, design: .rounded))
+                        .foregroundColor(Color("InfoColor"))
+                }
+            }
+            .frame(width: geometry.size.width, height:  geometry.size.height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        })
     }
 }
 
